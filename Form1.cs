e20ec6eb7800ab62bfdaae54d8584d892f4ebd74
hat3k <hat3k@mail.ru>
2025-10-04 06:09:10 +0300

    UI improvement - log divided into tabs

--- Form1.cs
+++ Form1.cs
@@ -24,7 +24,7 @@ namespace FLAC_Benchmark_H
     public partial class Form1 : Form
     {
         // Application version
-        public string programVersionCurrent = "1.7.3 build 20250928"; // Current app version
+        public string programVersionCurrent = "1.7.4 build 20251004"; // Current app version
         public string programVersionIgnored = null;                   // Previously ignored update
 
         // Hardware info
